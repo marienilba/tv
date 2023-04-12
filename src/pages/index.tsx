c7f01c24b102ffcbbaf72b3291cf2ceb27a18eef
Marie Rioux <mael.rioux@axopen.com>
2023-04-12 23:55:38 +0200

    metadata

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { ChannelCard } from "@/components/channel-card";
 import { channels } from "@/data/channels";
 import { Inter } from "next/font/google";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"], display: "swap" });
 
@@ -11,6 +12,13 @@ export default function Home() {
     <main
       className={`bg-pink-200 min-h-screen w-screen flex justify-center items-center ${inter.className}`}
     >
+      <Head>
+        <title>TV</title>
+        <meta
+          name="description"
+          content="Liste TV liens pour regarder en direct"
+        />
+      </Head>
       <section className="grid grid-cols-6 gap-x-4 gap-y-4">
         {channels.map((channel, index) => (
           <div
